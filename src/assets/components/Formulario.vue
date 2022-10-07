8219eb5ac5d3b02a72b14208666e2d54fe790e42
Sneikder1 <sneikdermendez@gmail.com>
2022-10-07 17:59:16 -0500

    hoy

--- src/assets/components/Formulario.vue
+++ src/assets/components/Formulario.vue
@@ -5,6 +5,7 @@
       tipo:"",
       urgente: false,
       proyectos:[],
+     
     }),
     methods: {
       registrarProyecto(){
@@ -12,18 +13,53 @@
           proyecto: this.proyecto,
           tipo: this.tipo,
           urgente: this.urgente,
+          completado: false,
         };
         this.proyectos.push(proyecto);
         
         this.proyecto = "";
         this.tipo = "";
         this.urgente = false;
+        
       },
+      cambiarEstado(proyecto,campo){
+        
+        //this.proyectos[id].urgente = !this.proyectos[id].urgente;
+        //console.log(proyecto, campo);
+        proyecto[campo] = !proyecto[campo];
+      }
+    },
+    computed:{
+      numeroProyectos(){
+        return this.proyectos.length;
+      },
+      porcentaje () {
+        let completados = 0;
+        this.proyectos.map(proyecto => {
+          if(proyecto.completado) completados++;
+
+        });
+        console.log((completados *100 )/this.numeroProyectos);
+
+        return;
+      }
     },
   };
 </script>
 <template>
-    <form @submit.prevent="registrarProyecto">
+    
+    
+    
+    <div class="row">
+      <div class="col-12 mb-4">
+        <h3 class="text-center">Progreso 0% </h3>
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" 
+          aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 50%"></div>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <form @submit.prevent="registrarProyecto">
       <div class="mb-3">
         <label class="form-label">Proyecto</label>
         <input v-model.trim="proyecto" type="text" class="form-control" required/>
@@ -45,25 +81,32 @@
       
       <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
-    <hr>
-    <div class="table-responsive">
-      <table class="table table-dark">
-        <thead>
+      </div>
+      <div class="col-12 col-md-8">
+        <h3>Total de proyectos: {{numeroProyectos}}</h3>
+        <div class="table-responsive">
+          <table class="table table-dark table-hover">
+            <thead>
           <tr>
             <th>#</th>
             <th>Proyecto</th>
             <th>Tipo</th>
             <th>Urgente</th>
+            <th>Completado</th>
           </tr>
-        </thead>
+            </thead>
+
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Laravel App</td>
-            <td>Larabel</td>
-            <td class="bg-success">Si / No</td>
+            <tr v-for="(proyecto, index) in proyectos" key="index">
+            <td>{{index + 1}}</td>
+            <td>{{proyecto.proyecto}}</td>
+            <td>{{proyecto.tipo}}</td>
+            <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger' ">{{proyecto.urgente ? "SI":"NO"}}</td>
+            <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger' ">{{proyecto.completado ? "Completado":"Incompleto"}}</td>
           </tr>
         </tbody>
       </table>
+    </div>
+      </div>
     </div>
 </template>
